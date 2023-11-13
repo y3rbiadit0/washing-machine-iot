52165cbf577a5884b33eb9cdc62cc990adb8929d
Franco N. Merenda <franco.merenda@gordiansoftware.com>
2023-11-14 00:50:15 +0100

    Added black

--- core/config.py
+++ core/config.py
@@ -2,9 +2,11 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     app_name: str = "Washing Machines - API"
     version: str = "v1"
+
 
 @lru_cache
 def settings() -> Settings:
--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from fastapi import FastAPI
 import api
 import core
 
-app = FastAPI(
-    title=core.settings().app_name,
-    version=core.settings().version
-)
+app = FastAPI(title=core.settings().app_name, version=core.settings().version)
 app.include_router(api.v1_router, prefix="/v1")
