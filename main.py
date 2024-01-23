db655021bfbc11d439b44bd2237d7344de71bfbb
Merenda Franco N <merenda.franco83@gmail.com>
2024-01-23 19:20:53 +0100

    Init db

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 import api
 import core
+from service.mongo_service.mongo_service import MongoDBService
 
+mongodb_service = MongoDBService.init_db()
 app = FastAPI(title=core.settings().app_name, version=core.settings().version)
 app.include_router(api.v1_router, prefix="/v1")
